8e90e177eda6e43606919b879a4f969dff49896b
d.plechystyy <d.plechystyy@ism-ukraine.com>
2013-08-28 16:19:57 +0300

    initial ShopProduct version

---
+++ lib/class.ShopProduct.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: d.plechystyy
+ * Date: 28.08.13
+ * Time: 15:52
+ * To change this template use File | Settings | File Templates.
+ */
+
+class ShopProduct
+{
+}
